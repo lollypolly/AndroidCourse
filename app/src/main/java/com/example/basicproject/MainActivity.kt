e5b01d2c4724540dccb819033ce41915a8b591f1
lollypolly <polina.mokeeva@mail.ru>
2020-09-20 01:12:14 +0300

    task2

--- app/src/main/java/com/example/basicproject/MainActivity.kt
+++ app/src/main/java/com/example/basicproject/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.basicproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val a : Byte = 8;
+
+        button2.setOnClickListener {
+            if (editText.visibility == View.INVISIBLE) {
+                editText.visibility = View.VISIBLE
+                button2.setText("Сохранить")
+            }
+                else if (editText.visibility == View.VISIBLE) {
+                editText.visibility = View.INVISIBLE
+                button2.setText("Редактировать...")
+            }
+
+
+            }
+
+
+        }
     }
-}
