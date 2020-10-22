b25b9c474d1d964657d6687a9cae0b82c002dbcb
lollypolly <polina.mokeeva@mail.ru>
2020-10-23 01:05:52 +0300

    hw-5-e

--- app/src/main/java/com/example/basicproject/MainActivity.kt
+++ app/src/main/java/com/example/basicproject/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.basicproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView.adapter = CatAdapter(
+            CatRepository.getCats(),
+            { cat: Cat, i: Int ->
+                if (cat.likes == R.drawable.ic_like) {
+                    cat.likes = R.drawable.ic_like_act
+                } else {
+                    cat.likes = R.drawable.ic_like
+                }
+                recyclerView.adapter?.notifyItemChanged(i)
+
+            },
+            {
+                var cat: Cat = it
+                var intent = Intent(this, CatProfile::class.java)
+                intent.putExtra("id", cat.id)
+                startActivity(intent)
+
+            }
+        )
+
+        recyclerView.addItemDecoration(SpaceItemDecoration(this))
+
+        swipe.setOnRefreshListener {
+            swipe.isRefreshing = false
+        }
     }
 }
+
