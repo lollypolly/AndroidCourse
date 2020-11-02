4c91717c3037418e0fcac2d444e3ef8bd812131d
lollypolly <polina.mokeeva@mail.ru>
2020-11-02 22:20:23 +0300

    hw-6

--- app/src/main/java/com/example/basicproject/MainActivity.kt
+++ app/src/main/java/com/example/basicproject/MainActivity.kt
@@ -1,12 +1,71 @@
 package com.example.basicproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.ListFragment
+import com.example.basicproject.cards.CardFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var cardFragment: CardFragment
+    lateinit var textFragment: TextFragment
+    lateinit var listFragment: ListFragment
+    lateinit var lastFragment: Fragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val a : Byte = 8;
+
+        val fragmentId = R.id.frame_layout
+        textFragment = TextFragment()
+        listFragment = ListFragment()
+        cardFragment = CardFragment()
+        supportFragmentManager.beginTransaction()
+            .add(fragmentId, textFragment)
+            .add(fragmentId, listFragment)
+            .add(fragmentId, cardFragment)
+            .hide(listFragment)
+            .hide(cardFragment)
+            .commit()
+        lastFragment = cardFragment
+
+        bottom_nav_bar.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.page_1 -> {
+                    changeFragment(textFragment)
+                    lastFragment = textFragment
+                    true
+                }
+                R.id.page_2 -> {
+                    changeFragment(listFragment)
+                    lastFragment = listFragment
+                    true
+                }
+                R.id.page_3 -> {
+                    changeFragment(cardFragment)
+                    lastFragment = cardFragment
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+    private fun changeFragment(Fragment: Fragment) {
+        if (Fragment != lastFragment)
+            if (
+                Fragment == textFragment && (lastFragment == listFragment || lastFragment == cardFragment) ||
+                Fragment == listFragment && lastFragment == cardFragment
+            )
+                supportFragmentManager.beginTransaction()
+                    .hide(lastFragment)
+                    .show(Fragment)
+                    .commit()
+            else
+                supportFragmentManager.beginTransaction()
+                    .hide(lastFragment)
+                    .show(Fragment)
+                    .commit()
     }
 }
